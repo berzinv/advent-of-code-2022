2934e34d09af4e36017c45a9fdd710eb1aae81f0
Vincent BERZIN <vincent.berzin>
2022-12-01 11:17:41 +0100

    Cleaning and unit tests

--- day1.scm
+++ day1.scm
@@ -1,26 +1,33 @@
-(import (only utilities file->lines sort-descending)
-        (only simple-loops do-list)
-        (only (chicken sort) sort)
-        (only srfi-1 take)
-        (only srfi-13 string-null?))
-
-(define calories (file->lines "input.day1.txt"))
-
-(define (sum-calories calories)
-  (let ((cumulative '())
-        (current 0))
-    (do-list (item calories)
-             (cond
-              ((string-null? item)
-               (begin
-                 (set! cumulative (cons current cumulative))
-                 (set! current 0)))
-              (else
-               (set! current (+ current (string->number item))))))
-    (begin
-      (set! cumulative (cons current cumulative))
-      (sort-descending cumulative))))
+(module
+ day1
+ (sum-calories answer1 answer2)
  
-(define calories-by-elves (sum-calories calories))
-(print (car calories-by-elves)) ; Answer 1
-(print (apply + (take calories-by-elves 3))) ; Answer 2
+ (import scheme
+         (only utilities file->lines sort-descending)
+         (only simple-loops do-list)
+         (only (chicken sort) sort)
+         (only srfi-1 take)
+         (only srfi-13 string-null?))
+
+ (define (sum-calories calories)
+   (let ((cumulative '())
+         (current 0))
+     (do-list (item calories)
+              (cond
+               ((string-null? item)
+                (begin
+                  (set! cumulative (cons current cumulative))
+                  (set! current 0)))
+               (else
+                (set! current (+ current (string->number item))))))
+     (begin
+       (set! cumulative (cons current cumulative))
+       (sort-descending cumulative))))
+ 
+ (define (answer1 calories)
+   (car (sum-calories calories)))
+
+ (define (answer2 calories)
+   (apply + (take (sum-calories calories) 3)))
+ 
+)
---
+++ day1.tests.scm
@@ -0,0 +1,18 @@
+(import test
+        utilities
+        (prefix day1 day1:))
+
+(define calories (file->lines "input.day1.txt"))
+
+(test-group "Day 1 test input"
+            (test "Answer 1" 24000 (day1:answer1 (file->lines "input.day1.test.txt")))
+            (test "Answer 2" 45000 (day1:answer2 (file->lines "input.day1.test.txt"))))
+
+
+(test-group "Day 1 real input"
+            (test "Answer 1"  68442 (day1:answer1 (file->lines "input.day1.txt")))
+            (test "Answer 2"  204837 (day1:answer2 (file->lines "input.day1.txt"))))
+
+
+(test-exit)
+
