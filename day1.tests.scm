eb5d9cd205099926699bfea0a988fca9c70496d0
Vincent BERZIN <vincent.berzin>
2022-12-01 11:25:05 +0100

    Cleaning

--- day1.tests.scm
+++ day1.tests.scm
@@ -2,16 +2,17 @@
         (only utilities file->lines)
         (prefix day1 day1:))
 
-(define calories (file->lines "input.day1.txt"))
+(define day1-test-input (file->lines "input.day1.test.txt"))
+(define day1-real-input (file->lines "input.day1.txt"))
 
 (test-group "Day 1 test input"
-            (test "Answer 1" 24000 (day1:answer1 (file->lines "input.day1.test.txt")))
-            (test "Answer 2" 45000 (day1:answer2 (file->lines "input.day1.test.txt"))))
+            (test "Answer 1" 24000 (day1:answer1 day1-test-input))
+            (test "Answer 2" 45000 (day1:answer2 day1-test-input)))
 
 
 (test-group "Day 1 real input"
-            (test "Answer 1"  68442 (day1:answer1 (file->lines "input.day1.txt")))
-            (test "Answer 2"  204837 (day1:answer2 (file->lines "input.day1.txt"))))
+            (test "Answer 1"  68442 (day1:answer1 day1-real-input))
+            (test "Answer 2"  204837 (day1:answer2 day1-real-input)))
 
 
 (test-exit)
