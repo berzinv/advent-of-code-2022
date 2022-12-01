68c5bda5eba478b532f46996f6226e531f2ac5eb
Vincent BERZIN <vincent.berzin>
2022-12-01 15:27:30 +0100

    Changed for a recursive version

--- day1.scm
+++ day1.scm
@@ -1,6 +1,6 @@
 (module
  day1
- (answer1 answer2)
+ (group-calories sum-calories answer1 answer2)
  
  (import scheme
          (only utilities file->lines sort-descending)
@@ -9,25 +9,38 @@
          (only srfi-1 take)
          (only srfi-13 string-null?))
 
- (define (sum-calories calories)
-   (let ((cumulative '())
-         (current 0))
-     (do-list (item calories)
-              (cond
-               ((string-null? item)
-                (begin
-                  (set! cumulative (cons current cumulative))
-                  (set! current 0)))
-               (else
-                (set! current (+ current (string->number item))))))
-     (begin
-       (set! cumulative (cons current cumulative))
-       (sort-descending cumulative))))
- 
+ ;; (define (sum-calories calories)
+ ;;   (let ((cumulative '())
+ ;;         (current 0))
+ ;;     (do-list (item calories)
+ ;;              (cond
+ ;;               ((string-null? item)
+ ;;                (begin
+ ;;                  (set! cumulative (cons current cumulative))
+ ;;                  (set! current 0)))
+ ;;               (else
+ ;;                (set! current (+ current (string->number item))))))
+ ;;     (begin
+ ;;       (set! cumulative (cons current cumulative))
+ ;;       (sort-descending cumulative))))
+
+(define (group-calories calories)
+  (let loop ((current '())
+             (accumulator '())
+             (calories calories))
+    (cond
+      ((null? calories) (cons current accumulator))
+      ((string=? "" (car calories)) (loop '() (cons current accumulator) (cdr calories)))
+      (else
+       (loop (cons (string->number (car calories)) current) accumulator (cdr calories))))))
+
+(define (sum-calories calories)
+  (sort-descending (map (lambda (lst) (apply + lst)) calories)))
+
  (define (answer1 calories)
-   (car (sum-calories calories)))
+   (car (sum-calories (group-calories calories))))
 
  (define (answer2 calories)
-   (apply + (take (sum-calories calories) 3)))
+   (apply + (take (sum-calories (group-calories calories)) 3)))
  
 )
